22025968929ac243696e2f2086eb2b8465a2e399
Lama Almuayli <93340105+LamaAlmuayli@users.noreply.github.com>
2023-10-22 11:42:16 +0300

    Daniyh's Pages

--- lib/main.dart
+++ lib/main.dart
@@ -15,12 +15,13 @@ import 'package:flutter_application_1/splash_Page.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
+      name: "TheraSense",
       options: FirebaseOptions(
-    apiKey: "AIzaSyAyaW86iJF-2Zh0n0aukpgzK7Z9ff5jaPs",
-    appId: "Y1:621591419878:android:401900529264c1499c1427",
-    messagingSenderId: "621591419878",
-    projectId: "therasense-391bb",
-  ));
+        apiKey: "AIzaSyAyaW86iJF-2Zh0n0aukpgzK7Z9ff5jaPs",
+        appId: "Y1:621591419878:android:401900529264c1499c1427",
+        messagingSenderId: "621591419878",
+        projectId: "therasense-391bb",
+      ));
 
   runApp(const MyApp());
 }
